9f3408e6a298e3fa0724359f377623b98ca5b0b0
pongloongyeat <pongloongyeat@gmail.com>
2021-03-25 01:48:43 +0800

    Make example more readable

--- examples/simple-dialog.sh
+++ examples/simple-dialog.sh
@@ -4,4 +4,6 @@ echo "Show a simple message to the user after some operations".
 
 sleep 3
 
-diorite --primary-text="It looks like the script file is done running!" --secondary-text="You may now resume whatever you were doing." --image-icon-name="process-completed"
+diorite --primary-text="It looks like the script file is done running!" \
+        --secondary-text="You may now resume whatever you were doing." \
+        --image-icon-name="process-completed"
